3487fe669c693df3da1a348d632b454821e22e3a
Nikhita Medayhal <nikhita.medayhal@yahoo.com>
2025-11-29 19:20:57 +0530

    Updated code -- Encodes categorical columns

--- model/train.py
+++ model/train.py
@@ -4,34 +4,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
 data = pd.read_csv("//Users//nikhita//salary_prediction//data//Employers_data.csv")
 df = pd.DataFrame(data)
-categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-encoder = OneHotEncoder(sparse_output=False)
+#print(df)
+categorical_features = ['Education_Level'] 
+numeric_features = ['Experience_Years']     
 
-one_hot_encoded = encoder.fit_transform(df[categorical_columns])
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(drop='first'), categorical_features)
+    ],
+    remainder='passthrough'
+)
 
-one_hot_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(categorical_columns))
 
-df_encoded = pd.concat([df, one_hot_df], axis=1)
+X = df[["Experience_Years","Education_Level"]]
+Y = df["Salary"]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-df_encoded = df_encoded.drop(categorical_columns, axis=1)
-print(f"Encoded Employee data : \n{df_encoded}")
+X_train_enc = preprocessor.fit_transform(X_train)
+X_test_enc  = preprocessor.transform(X_test)
 
-X = df_encoded[['Experience_Years','Gender']]
-Y = df_encoded['Salary']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0.42)
 model = LinearRegression()
-model.fit(X_train, Y_train)
-Y_pred = model.predict(X_test)
-x=model.predict(pd.DataFrame({'Experience_Years': [6]},{'Gender': '24'}))
-print(x)
-mae = mean_absolute_error(Y_test, Y_pred)
-mse = mean_squared_error(Y_test, Y_pred)
-rmse = mse**0.5
-r2 = r2_score(Y_test, Y_pred)
-print("MAE:", mae)
-print("MSE:", mse)
-print("RMSE:", rmse)
-print("R2 Score:", r2)
-print("Accuracy Percentage:", r2 * 100, "%")
+model.fit(X_train_enc, Y_train)
+
+new_pred = pd.DataFrame({
+'Experience_Years' : [6],
+'Education_Level' : ['Master']})
+new_pred_enc = preprocessor.transform(new_pred)
+Y_pred = model.predict(new_pred_enc)
+print(Y_pred)
